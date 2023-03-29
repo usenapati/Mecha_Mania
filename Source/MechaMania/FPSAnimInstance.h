64dc69e7ed027e6867e35fd740d5e22a28e2ac0a
Udayan Senapati <udayansenapati@gmail.com>
2023-03-28 20:10:12 -0400

    AnimInstance and Control Rig (Not Connected)

---
+++ Source/MechaMania/FPSAnimInstance.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "Animation/AnimInstance.h"
+#include "FPSAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHAMANIA_API UFPSAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UFPSAnimInstance();
+
+protected:
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	virtual void CurrentWeaponChanged(class AWeapon* NewWeapon, const class AWeapon* OldWeapon);
+	virtual void SetVars(const float DeltaTime);
+	virtual void CalculateWeaponSway(const float DeltaTime);
+
+public:
+	/*
+	 * REFERENCES
+	 */
+	UPROPERTY(BlueprintReadWrite, Category = "Anim")
+	class AFPSCharacter* Character;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Anim")
+	class USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Anim")
+	class AWeapon* CurrentWeapon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Anim")
+	FIKProperties IKProperties;
+
+	/*
+	 * IK VARS
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Anim")
+	FTransform CameraTransform;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Anim")
+	FTransform RelativeCameraTransform;
+};
